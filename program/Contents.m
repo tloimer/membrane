efe345e40ef62351b7c7528e2e82ee2b5aec4eb2
Thomas Loimer <thomas.loimer@tuwien.ac.at>
2015-04-07 15:21:55 +0200

    Add help-file Contents.m

---
+++ program/Contents.m
@@ -0,0 +1,36 @@
+% Flow through porous membranes
+% Version asym 02-Apr-2015
+%
+% Setup.
+%   substance     - Material properties of a substance.
+%   fmodel        - Two-phase flow model.
+%   membrane      - A homogeneous membrane.
+%   mstackstruct  - A stack of membranes, each consisting of several layers.
+%
+% Compute.
+%   mnumadiabat   - Mass flux for adiabatic flow through layered membranes.
+%   asym          - Upstream pressure for adiabatic flow through a MSTACKSTRUCT.
+%   mgaseous      - Mass flux for isothermal gaseous flow.
+%   misotherm     - Isothermal mass flux through a homogeneous membrane.
+%   mlinear       - Mass flux from linear theory through a homogeneous membrane.
+%   See also functions in MSTACKSTRUCT, see below.
+%
+% Plot.
+%   beginpgfplot  - Write the begin of a pgfplot.
+%   addcoords     - Add data to a pgfplot.
+%   endpgfplot    - Close a pgfplot.
+%   pTplot        - Plot p-T diagram.
+%   pTzplots      - Plot p-z, T-z and, optionally, q-z diagrams.
+%   Tsplot        - Plot a T-s Diagram.
+%   See also functions in MSTACKSTRUCT, see below.
+%
+% Utilities.
+%   downstreamstate - Return a STATE struct.
+%   findinterval    - Find an interval in which a function changes sign.
+%   findzero        - Find the zero of a function.
+%   flowsetup       - Setup flow properties.
+%   solverstruct    - Construct a solver struct.
+%
+% See also mstackstruct>mfluxliquid, mstackstruct>mfluxviscous,
+%          mstackstruct>mfluxknudsen, mstackstruct>printsetup,
+%          mstackstruct>plotsolution, mstackstruct>printsolution.
