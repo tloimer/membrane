684776ea9ba7aeea57ad6995602af9ea3dd54b52
Thomas Loimer <thomas.loimer@tuwien.ac.at>
2015-11-27 15:34:45 +0100

    readdata.m reads text-files written with Windows
    Windows applications may write utf16-data (2 bytes for one character)
    with a BOM (Byte-Order Marker) at the beginning of the file.
    This is now cared for.

--- program/readdata.m
+++ program/readdata.m
@@ -12,7 +12,7 @@ function data = readdata(file)
 %
 %  Suppose a file meas.tsv contains the tab-delimited text
 %     Date       substance  p1   p2
-%     12.3.2015  nitrogen   1.2  0.995  
+%     12.3.2015  nitrogen   1.2  0.995
 %     15.3.2015  butane     1.3  1
 %  then DATA = READDATA('meas.tsv') returns a 1x1 struct DATA with the
 %  fields DATA.Date, DATA.substance, DATA.p1 and DATA.p2.
@@ -26,6 +26,21 @@ del = '\t';
 
 fid = fopen(file);
 
+% Check the encoding - rrgh, windows
+% Read the first two bytes and, if it is a BOM, set the appropriate
+% encoding
+len = fread(fid,[1,2]);
+if (len(1,1) == 255 && len(1,2) == 254)
+  fclose(fid);
+  warning('off','MATLAB:iofun:UnsupportedEncoding');
+  fopen(file,'r','l','UTF16-LE');
+elseif (len(1,1) == 254 && len(1,2) == 255)
+  warning('off','MATLAB:iofun:UnsupportedEncoding');
+  fopen(file,'r','b','UTF16-BE');
+else
+  frewind(fid);
+end
+
 % Get the variable names.
 %		textscan cares for \r\n, ignores the \r, if present
 [line, pos] = textscan(fid,'%s',1,'Delimiter','\n');
@@ -34,20 +49,23 @@ len = size(name,2);
 
 % Read the second line for testing whether a column is numeric or string.
 line = textscan(fid,'%s',len,'Delimiter',del);
-fmt(1:2*len) = '%';
 
 % Loop over columns.
 %   1. Set read format, numeric or string.
 %   2. Sanitize variable names.
+fmt = '';
 for i = 1:len
   %  Set read format, numeric or string.
-  if any(isstrprop(line{1}{i},'alpha')) || sum(line{1}{i} == '.') > 1
-    fmt(2*i) = 's';
+  if isempty(name{i}) % or: strcmp('',name{i})
+    fmt = [fmt '%*s'];  %Discard columns with empty headers
+  elseif sum(line{1}{i}=='.') < 2 ...
+	 && all(isstrprop(line{1}{i},'digit') | line{1}{i} == '.')
+    fmt = [fmt '%n'];
   else
-    fmt(2*i) = 'n';
+    fmt = [fmt '%s'];
   end
   %  Sanitize variable names.
-  if ~isvarname(name{i})
+  if (~isempty(name{i}) && ~isvarname(name{i}))
     name{i} = name{i}(isstrprop(name{i},'alphanum'));
     if ~isvarname(name{i})
       error('Column %d header ''%s'' is not a valid matlab variable name.',...
@@ -56,6 +74,7 @@ for i = 1:len
   end
 end
 
+name = name(~strcmp('',name)); % Discard columns with empty headers
 fseek(fid,pos,'bof');	% Seek to postition pos from beginning of file.
 % d = textscan(fid,fmt,'Delimiter',del,'CommentStyle','#');
 % data = cell2struct(d, name, 2)
